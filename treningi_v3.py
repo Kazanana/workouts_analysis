54a9000af78c00e2ae2f0dc258740d7be24e2bab
domino <kazana.dominik@gmail.com>
2024-12-07 00:19:41 +0100

    all exercises filtered

--- treningi_v3.py
+++ treningi_v3.py
@@ -5,35 +5,39 @@ import sqlite3
 def split_workouts(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
-    
-    # Regular expression to match dd.mm.yyyy format
-    # This pattern matches a line with the date and place (dd.mm.yyyy place),
-    # followed by one or more lines of exercises.
+
     pattern = r'(\d{2}\.\d{2}\.\d{4} .+?)(?=(\d{2}\.\d{2}\.\d{4} )|\Z)'
-    
-    # Find all matches in the file content
+
     matches = re.findall(pattern, content, re.DOTALL)
     
-    # Extract only the full entry from each match
     entries = [match[0].strip() for match in matches]
     
     return entries
 
-def insert_WorkoutComment(date,place):
+def insert_WorkoutComment(date,place, dry_run = True):
+    if dry_run:
+        query = "INSERT INTO WorkoutComment (date, comment) VALUES (?, ?)"
+        data = (date, place)
+        print(f"Query: {query}\n Data: {data}")
+    else:
+        query = "INSERT INTO WorkoutComment (date, comment) VALUES (?, ?)"
+        data = (date, place)
+        cursor.execute(query, data)
+        conn.commit()
 
-    query = "INSERT INTO WorkoutComment (date, comment) VALUES (?, ?)"
-    data = (date, place)
-    cursor.execute(query, data)
-    conn.commit()
-
-def insert_training_log(exercise_name,date,reps,weight):
+def insert_training_log(exercise_name,date,reps,weight, dry_run = True):
     # reps and weight
-    cursor.execute(f"select _id from exercise where name like '{exercise_name}';")
-    exercise_id = cursor.fetchall()
-    query = "insert into training_log (exercise_id,date,reps,metric_weight) values(?,?,?,?);"
-    data = (exercise_id, date, reps, weight)
-    cursor.execute(query, data)
-    conn.commit()
+    if dry_run:
+        query = "insert into training_log (exercise_id,date,reps,metric_weight) values(?,?,?,?);"
+        data = (exercise_id, date, reps, weight)
+        print(f"Query: {query}\n Data: {data}")
+    else:
+        cursor.execute(f"select _id from exercise where name like '{exercise_name}';")
+        exercise_id = cursor.fetchall()
+        query = "insert into training_log (exercise_id,date,reps,metric_weight) values(?,?,?,?);"
+        data = (exercise_id, date, reps, weight)
+        cursor.execute(query, data)
+        conn.commit()
 
 def split_sets_into_reps(ex):
     sets_list = []
@@ -60,6 +64,45 @@ exercise_entries = split_workouts(file_path)
 # conn = sqlite3.connect(db_path)
 # cursor = conn.cursor()
 
+exercise_dict = {"exercises": [
+    {
+        "name": "Chest Dip",
+        "regex": r'dip:|Dip:|dipy:|Dipy:'
+    },
+    {
+        "name": "Pull Up",
+        "regex": r'Pull up:|pull up:'
+    },
+    {
+        "name": "Handstand Pushup",
+        "regex": r'(?!.*ściana)(Hs|HS) (pu|push up|Pu)'
+    },
+    {
+        "name": "Handstand Pushup Wall",
+        "regex": r'(Hs|HS) (pu|push up|Pu).*ściana'
+    },
+    {
+        "name": "Muscle Up",
+        "regex": r'Muscle up'
+    },
+    {
+        "name": "Chin Up",
+        "regex": r'Chin up'
+    },
+    {
+        "name": "Barbell Squat",
+        "regex": r'Przysiad'
+    },
+    {
+        "name": "Neutral Chin Up",
+        "regex": r'Neutral'
+    },
+    {
+        "name": "Handstand",
+        "regex": r'(?!.*:).*hs.*|(?!.*:).*HS.*|(?!.*:).*Hs.*'
+    }
+]}
+
 for entry in exercise_entries:
     # need to split like that because entry is a string
     date_and_place = entry.splitlines()[0]
@@ -71,13 +114,39 @@ for entry in exercise_entries:
 
     place = date_and_place.split(' ', 1)[1]
     print(date_and_place)
+    # insert insert_WorkoutComment(data, place)
     for ex in exercises:
-        matches_dip = re.findall(r'dip:|Dip:|dipy:|Dipy:', ex)
-        matches_pullup = re.findall(r'Pull up:|pull up:', ex)
-        if matches_dip:
+        matches_ChestDip = re.findall(r'dip:|Dip:|dipy:|Dipy:', ex)
+        matches_PullUp = re.findall(r'Pull up:|pull up:', ex)
+        matches_HS_PU = re.findall(r'(?!.*ściana)(Hs|HS) (pu|push up|Pu)', ex)
+        matches_HS_PU_Wall = re.findall(r'(Hs|HS) (pu|push up|Pu).*ściana', ex)
+        matches_MuscleUp = re.findall(r'Muscle up', ex)
+        matches_ChinUp = re.findall(r'Chin up', ex)
+        matches_BarbellSquat = re.findall(r'Przysiad', ex)
+        matches_NeutralChinUp = re.findall(r'Neutral', ex)
+        matches_HS = re.findall(r'(?!.*:).*hs.*|(?!.*:).*HS.*|(?!.*:).*Hs.*', ex)
+
+        if matches_ChestDip:
             print(split_sets_into_reps(ex))
-        elif matches_pullup:
+        elif matches_PullUp:
             print(split_sets_into_reps(ex))
+        elif matches_HS_PU:
+            print(split_sets_into_reps(ex))
+        elif matches_HS_PU_Wall:
+            print(split_sets_into_reps(ex))
+        elif matches_MuscleUp:
+            print(split_sets_into_reps(ex))
+        elif matches_ChinUp:
+            print(split_sets_into_reps(ex))
+        elif matches_BarbellSquat:
+            print(split_sets_into_reps(ex))
+        elif matches_NeutralChinUp:
+            print(split_sets_into_reps(ex))
+        elif matches_HS:
+            print("HANDSTAND PRACTICE")
+        else:
+            continue
+
     print("-----------------------------------------------")
 # insert_WorkoutComment(date, place)
 # print(f"PLACE: {date_and_place}\n ENTRIES: {exercises}")
